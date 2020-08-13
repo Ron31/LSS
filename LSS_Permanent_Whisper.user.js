3f807ca502adf1bb56a9a574ed9c0d97eebdb94b
Cr4zyc4k3 <arne.setzer@web.de>
2020-08-13 20:13:44 +0200

    Fixed spelling mistakes

--- LSS_Permanent_Whisper.user.js
+++ LSS_Permanent_Whisper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LSS Chat permanent whisper
 // @version      1.0
-// @description  Enables to whisper permanent in alliance chat.
+// @description  Enables permanent whisper in alliance chat.
 // @author       Crazycake
 // @include      /^https?:\/\/(?:w{3}\.)?(?:operacni-stredisko\.cz|alarmcentral-spil\.dk|leitstellenspiel\.de|missionchief\.gr|(?:missionchief-australia|missionchief|hatakeskuspeli|missionchief-japan|missionchief-korea|nodsentralspillet|meldkamerspel|operador193|jogo-operador112|jocdispecerat112|dispecerske-centrum|112-merkez|dyspetcher101-game)\.com|missionchief\.co\.uk|centro-de-mando\.es|centro-de-mando\.mx|operateur112\.fr|operatore112\.it|operatorratunkowy\.pl|dispetcher112\.ru|larmcentralen-spelet\.se)\/?$/
 // @grant        none
