903e33fba878ff6bbfd834389ccfd86631ab4d6a
Cr4zyc4k3 <arne.setzer@web.de>
2020-08-16 01:24:33 +0200

    Added example pictures

--- LSS_Permanent_Whisper.user.js
+++ LSS_Permanent_Whisper.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        LSS Chat permanent whisper
-// @version     1.2
+// @version     1.3
 // @description Enables permanent whisper in alliance chat.
 // @author      Crazycake
 // @include     /^https?:\/\/(?:w{3}\.)?(?:operacni-stredisko\.cz|alarmcentral-spil\.dk|leitstellenspiel\.de|missionchief\.gr|(?:missionchief-australia|missionchief|hatakeskuspeli|missionchief-japan|missionchief-korea|nodsentralspillet|meldkamerspel|operador193|jogo-operador112|jocdispecerat112|dispecerske-centrum|112-merkez|dyspetcher101-game)\.com|missionchief\.co\.uk|centro-de-mando\.es|centro-de-mando\.mx|operateur112\.fr|operatore112\.it|operatorratunkowy\.pl|dispetcher112\.ru|larmcentralen-spelet\.se)\/?$/
