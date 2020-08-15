b906c9cb19ecd24369714d92bb34be27e7a57dde
Cr4zyc4k3 <arne.setzer@web.de>
2020-08-16 00:05:25 +0200

    Added awesomeplete to autocomplete

--- LSS_Permanent_Whisper.user.js
+++ LSS_Permanent_Whisper.user.js
@@ -1,10 +1,14 @@
 // ==UserScript==
-// @name         LSS Chat permanent whisper
-// @version      1.0
-// @description  Enables permanent whisper in alliance chat.
-// @author       Crazycake
-// @include      /^https?:\/\/(?:w{3}\.)?(?:operacni-stredisko\.cz|alarmcentral-spil\.dk|leitstellenspiel\.de|missionchief\.gr|(?:missionchief-australia|missionchief|hatakeskuspeli|missionchief-japan|missionchief-korea|nodsentralspillet|meldkamerspel|operador193|jogo-operador112|jocdispecerat112|dispecerske-centrum|112-merkez|dyspetcher101-game)\.com|missionchief\.co\.uk|centro-de-mando\.es|centro-de-mando\.mx|operateur112\.fr|operatore112\.it|operatorratunkowy\.pl|dispetcher112\.ru|larmcentralen-spelet\.se)\/?$/
-// @grant        none
+// @name        LSS Chat permanent whisper
+// @version     1.2
+// @description Enables permanent whisper in alliance chat.
+// @author      Crazycake
+// @include     /^https?:\/\/(?:w{3}\.)?(?:operacni-stredisko\.cz|alarmcentral-spil\.dk|leitstellenspiel\.de|missionchief\.gr|(?:missionchief-australia|missionchief|hatakeskuspeli|missionchief-japan|missionchief-korea|nodsentralspillet|meldkamerspel|operador193|jogo-operador112|jocdispecerat112|dispecerske-centrum|112-merkez|dyspetcher101-game)\.com|missionchief\.co\.uk|centro-de-mando\.es|centro-de-mando\.mx|operateur112\.fr|operatore112\.it|operatorratunkowy\.pl|dispetcher112\.ru|larmcentralen-spelet\.se)\/?$/
+// @grant       none
+// @require     https://cdn.jsdelivr.net/npm/awesomplete@1.1.5/awesomplete.js
+// @resource    https://cdn.jsdelivr.net/npm/awesomplete@1.1.5/awesomplete.css
+// @grant       GM_addStyle
+// @grant       GM_getResourceText
 
 // ==/UserScript==
 
@@ -12,10 +16,11 @@
     'use strict';
 
     var chathead = document.getElementById("chat_panel_heading");
-    chathead.insertAdjacentHTML('beforebegin','<div id="WhisperDiv" class="pull-right" style="padding-top:2%;padding-right:2%;padding-left:1%"> Permanent whisper <input type="checkbox" id="Whisper"></div>');
+    chathead.insertAdjacentHTML('beforebegin','<div id="WhisperDiv" class="pull-right" style="padding-top:2%;padding-right:2%;padding-left:1%"> Permanent whisper to <input id="WhisperUserSelect"> <input type="checkbox" id="Whisper"></div>');
 
 
     var allianceinfo = {};
+    var AllianceUsersDatalistArray = [];
     $.getJSON('/api/allianceinfo', function(data){
     allianceinfo = data;
     var allianceUsers = [];
@@ -23,6 +28,10 @@
     {
         allianceUsers.push(allianceinfo.users[i].name);
     }
+    var whisperUserSelect = document.getElementById("WhisperUserSelect");
+    var awesomplete = new Awesomplete (whisperUserSelect)
+    awesomplete.list = allianceUsers;
+
                
     });
 
