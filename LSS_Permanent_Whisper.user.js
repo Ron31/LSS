2c0c2963b9bef6b5810db39970a4b1846163f47e
Cr4zyc4k3 <arne.setzer@web.de>
2020-08-19 16:35:29 +0200

    Added option to show only online users.

--- LSS_Permanent_Whisper.user.js
+++ LSS_Permanent_Whisper.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        LSS Chat permanent whisper
-// @version     1.4.1
+// @version     1.5
 // @description Enables permanent whisper in alliance chat.
 // @author      Crazycake
 // @include     /^https?:\/\/(?:w{3}\.)?(?:operacni-stredisko\.cz|alarmcentral-spil\.dk|leitstellenspiel\.de|missionchief\.gr|(?:missionchief-australia|missionchief|hatakeskuspeli|missionchief-japan|missionchief-korea|nodsentralspillet|meldkamerspel|operador193|jogo-operador112|jocdispecerat112|dispecerske-centrum|112-merkez|dyspetcher101-game)\.com|missionchief\.co\.uk|centro-de-mando\.es|centro-de-mando\.mx|operateur112\.fr|operatore112\.it|operatorratunkowy\.pl|dispetcher112\.ru|larmcentralen-spelet\.se)\/?$/
@@ -11,20 +11,32 @@
 (function() {
     'use strict';
 
+    //Config
+    var showOnlyOnlineUsers = false;
+
     var chathead = document.getElementById("chat_panel_heading");
     chathead.insertAdjacentHTML('beforebegin',
     '<div id="WhisperDiv" class="pull-right panel-heading"> Permanent whisper to <input id="whisperUser" list="whisperUserList" style="color:black"><datalist id="whisperUserList"></datalist> <input type="checkbox" id="Whisper"></div>');
    
 
     var allianceinfo = {};
-    var AllianceUsersDatalistArray = [];
     var whisperUserList = document.getElementById("whisperUserList");
     $.getJSON('/api/allianceinfo', function(data){
     allianceinfo = data;
     var allianceUsers = [];
     for(var i = 0; i < allianceinfo.users.length ; i++)
     {
-        allianceUsers.push(allianceinfo.users[i].name);
+        if(showOnlyOnlineUsers)
+        {
+            if(allianceinfo.users[i].online)
+            {
+                allianceUsers.push(allianceinfo.users[i].name);
+            }
+        }
+        else
+        {
+            allianceUsers.push(allianceinfo.users[i].name);
+        }
     }
     allianceUsers.forEach(function(item){
         var option = document.createElement('option');
