460847d2582c83c77d1cdbb88204941385ef2c3c
Cr4zyc4k3 <arne.setzer@web.de>
2020-08-20 17:30:45 +0200

    Added icon support.

--- LSS_Permanent_Whisper.user.js
+++ LSS_Permanent_Whisper.user.js
@@ -8,74 +8,89 @@
 // @updateURL   https://github.com/Cr4zyc4k3/LSS/raw/master/LSS_Permanent_Whisper.user.js
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     //Config
     var showOnlyOnlineUsers = false;
+    var enableIcons = false;
 
     var chathead = document.getElementById("chat_panel_heading");
     chathead.insertAdjacentHTML('beforebegin',
-    '<div id="WhisperDiv" class="pull-right panel-heading"> Permanent whisper to <input id="whisperUser" list="whisperUserList" style="color:black"><datalist id="whisperUserList"></datalist> <input type="checkbox" id="Whisper"></div>');
-   
+        '<div id="WhisperDiv" class="pull-right panel-heading"> Permanent whisper to <input id="whisperUser" list="whisperUserList" style="color:black"><datalist id="whisperUserList"></datalist> <input type="checkbox" id="Whisper"></div>');
+
 
     var allianceinfo = {};
     var whisperUserList = document.getElementById("whisperUserList");
-    $.getJSON('/api/allianceinfo', function(data){
-    allianceinfo = data;
-    var allianceUsers = [];
-    for(var i = 0; i < allianceinfo.users.length ; i++)
-    {
-        if(showOnlyOnlineUsers)
-        {
-            if(allianceinfo.users[i].online)
-            {
-                allianceUsers.push(allianceinfo.users[i].name);
+    $.getJSON('/api/allianceinfo', function (data) {
+        allianceinfo = data;
+        var allianceUsers = [];
+        for (var i = 0; i < allianceinfo.users.length; i++) {
+            if (showOnlyOnlineUsers) {
+                if (allianceinfo.users[i].online) {
+                    allianceUsers.push(allianceinfo.users[i].name);
+                }
+            }
+            else {
+                allianceUsers.push([allianceinfo.users[i].name, allianceinfo.users[i].roles.toString()]);
             }
         }
-        else
-        {
-            allianceUsers.push(allianceinfo.users[i].name);
-        }
-    }
-    allianceUsers.forEach(function(item){
-        var option = document.createElement('option');
-        option.value = item;
-        whisperUserList.appendChild(option);
-    })
+        //console.log(allianceUsers);
+        var i = 0;
+        allianceUsers.forEach(function (item) {
+            var option = document.createElement('option');
+            option.value = item[i, 0];
+
+            if (enableIcons) {
+                if (item[i, 1].includes("Verbands-Admin")) {
+                    option.innerHTML = option.value + ' ♛'
+                }
+                else if (item[i, 1].includes("Verbands-Co-Admin")) {
+                    option.innerHTML = option.value + ' ♛'
+                }
+                else if (item[i, 1].includes("Sprechwunsch-Admin")) {
+                    option.innerHTML = option.value + ' 📣'
+                }
+                else if (item[i, 1].includes("Lehrgangsmeister")) {
+                    option.innerHTML = option.value + ' 🕮'
+                }
+                else if (item[i, 1].includes("Finanzminister")) {
+                    option.innerHTML = option.value + ' 💰'
+                }
+                else if (item[i, 1].includes("Aufsichtsrat")) {
+                    option.innerHTML = option.value + ' 🔍'
+                }
+            }
+            i++;
+            whisperUserList.appendChild(option);
+        })
     });
 
 
-    function ToggleWhisper()
-    {
+    function ToggleWhisper() {
         var checkbox = document.getElementById("Whisper");
         var chatinhalt = document.getElementById("alliance_chat_message").value;
         var whisperUser = document.getElementById("whisperUser").value;
 
-        if(checkbox.checked == true)
-        {
-            if(!chatinhalt.includes("/w"))
-            {
+        if (checkbox.checked == true) {
+            if (!chatinhalt.includes("/w")) {
                 var w = "/w " + whisperUser + " ";
                 var chatinhalt_old = chatinhalt;
                 chatinhalt = w.concat(chatinhalt_old);
                 document.getElementById("alliance_chat_message").value = chatinhalt;
             }
         }
-        else
-        {
-            document.getElementById("alliance_chat_message").value = chatinhalt.replace("/w ","").replace(whisperUser + " ", "");
+        else {
+            document.getElementById("alliance_chat_message").value = chatinhalt.replace("/w ", "").replace(whisperUser + " ", "");
         }
     }
 
     var ToggleListener = document.querySelector("#Whisper");
-    if(ToggleListener)
-    {
-        ToggleListener.addEventListener("click",ToggleWhisper);
+    if (ToggleListener) {
+        ToggleListener.addEventListener("click", ToggleWhisper);
     }
     var ReloadListener = document.querySelector("#new_alliance_chat");
-    if(ReloadListener)
-    {
+    if (ReloadListener) {
         ReloadListener.addEventListener("submit", ToggleWhisper);
     }
 })();
