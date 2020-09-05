b5ee834fad959f5442d2b7622f85ba7ed1924bed
Cr4zyc4k3 <arne.setzer@web.de>
2020-09-05 11:37:43 +0200

    Added states via popup

--- LSS_mission_in_area_check.user.js
+++ LSS_mission_in_area_check.user.js
@@ -13,9 +13,15 @@
 
     //config
     var useOwnCoordinates = false;
-    var borderCoordinates = [[6.077742099761963, 50.90677642822277], [6.139449119567871, 50.901950836181641], [6.149639129638672, 50.932571411132812], [6.197109222412053, 50.953559875488281], [6.228148937225455, 50.943500518798942], [6.229781150817985, 50.920940399169979], [6.201058864593563, 50.897159576415959], [6.220118999481201, 50.886661529541129], [6.228139877319393, 50.860610961914176], [6.298820018768311, 50.885829925537166], [6.32427978515625, 50.867900848388729], [6.313479900360164, 50.852500915527344], [6.344958782196159, 50.834781646728629], [6.333299160003776, 50.830631256103516], [6.340498924255428, 50.815849304199219], [6.300570964813176, 50.788150787353572], [6.32017993927002, 50.77008056640625], [6.354650020599365, 50.790050506591854], [6.35053205490118, 50.767391204834041], [6.363470077514592, 50.756580352783317], [6.345469951629696, 50.755958557128906], [6.312428951263541, 50.717269897460938], [6.308847904205379, 50.687141418457088], [6.27944993972784, 50.674800872802734], [6.336188793182373, 50.650680541992188], [6.377129077911491, 50.667079925537223], [6.385397911071891, 50.641139984130973], [6.409420013427734, 50.641941070556754], [6.410729885101432, 50.627010345459041], [6.380990028381404, 50.622291564941463], [6.393350124359188, 50.618961334228572], [6.388629913330078, 50.603839874267578], [6.413340091705322, 50.597171783447266], [6.38959789276123, 50.592658996581974], [6.396599769592285, 50.581680297851619], [6.36250209808361, 50.558170318603629], [6.364560127258414, 50.535869598388786], [6.352869987487907, 50.531761169433707], [6.376927852630615, 50.532550811767635], [6.366659164428768, 50.513610839843807], [6.253439903259221, 50.494831085205135], [6.180831909179688, 50.533061981201172], [6.175640106201229, 50.559852600097599], [6.278378963470516, 50.616397857665959], [6.209949970245475, 50.630920410156307], [6.173087120056209, 50.621433258056697], [6.159657955169621, 50.643730163574332], [6.178049087524414, 50.644531249999943], [6.187379837036133, 50.678600311279297], [6.210578918457031, 50.694549560546875], [6.18271017074585, 50.734630584716911], [6.199257850647029, 50.757850646972656], [6.172968864440975, 50.779270172119254], [6.189321041107235, 50.806289672851562], [6.065680980682316, 50.808109283447266], [6.050435066223258, 50.85033035278326], [6.0738401412965, 50.846858978271484], [6.087838172912654, 50.869640350341911], [6.077742099761963, 50.90677642822277]];
+    var borderCoordinates = [];
 
-
+    if(borderCoordinates.length > 0)
+    {
+        setLS(borderCoordinates);
+    }
+    else if(borderCoordinates.length = 0 && JSON.parse(localStorage.getItem('LSS_mission_in_area_check')) !== 'undefined' && JSON.parse(localStorage.getItem('LSS_mission_in_area_check')) !== null ){
+        borderCoordinates = getLS();
+    }
 
     var url = window.location.pathname;
     if (window.top == window.self && !useOwnCoordinates) {
@@ -44,8 +50,6 @@
             var states = [];
             states = data.features;
             for (var i = 0; i < states.length; i++) {
-                //console.log(states[i].properties.id + " " + states[i].properties.name);
-                //console.log(states[i].geometry.coordinates[0]);
                 var option = document.createElement('option');
                 option.value = states[i].properties.name;
                 option.innerHTML = states[i].properties.name;
@@ -55,46 +59,60 @@
                 document.getElementById("stateSelect").value;
                 switch (document.getElementById("stateSelect").value) {
 
-                    case "Baden-Württemberg": borderCoordinates = states[0].geometry.coordinates[0]; break;
-                    case "Bayern": borderCoordinates = states[1].geometry.coordinates[0]; break;
-                    case "Berlin": borderCoordinates = states[2].geometry.coordinates[0]; break;
-                    case "Brandenburg": borderCoordinates = states[3].geometry.coordinates[0]; break;
-                    case "Bremen": borderCoordinates = states[4].geometry.coordinates[0]; break;
-                    case "Hamburg": borderCoordinates = states[5].geometry.coordinates[0]; break;
-                    case "Hessen": borderCoordinates = states[6].geometry.coordinates[0]; break;
-                    case "Mecklenburg-Vorpommern": borderCoordinates = states[7].geometry.coordinates[0]; break;
-                    case "Niedersachsen": borderCoordinates = states[8].geometry.coordinates[0]; break;
-                    case "Nordrhein-Westfalen": borderCoordinates = states[9].geometry.coordinates[0]; break;
-                    case "Rheinland-Pfalz": borderCoordinates = states[10].geometry.coordinates[0]; break;
-                    case "Saarland": borderCoordinates = states[11].geometry.coordinates[0]; break;
-                    case "Sachsen-Anhalt": borderCoordinates = states[12].geometry.coordinates[0]; break;
-                    case "Sachsen": borderCoordinates = states[13].geometry.coordinates[0]; break;
-                    case "Schleswig-Holstein": borderCoordinates = states[14].geometry.coordinates[0]; break;
-                    case "Thüringen": borderCoordinates = states[15].geometry.coordinates[0]; break;
+                    case "Baden-Württemberg": window.top.borderCoordinates = states[0].geometry.coordinates[0]; break;
+                    case "Bayern": window.top.borderCoordinates = states[1].geometry.coordinates[0]; break;
+                    case "Berlin": window.top.borderCoordinates = states[2].geometry.coordinates[0]; break;
+                    case "Brandenburg": window.top.borderCoordinates = states[3].geometry.coordinates[0]; break;
+                    case "Bremen": window.top.borderCoordinates = states[4].geometry.coordinates[0]; break;
+                    case "Hamburg": window.top.borderCoordinates = states[5].geometry.coordinates[0]; break;
+                    case "Hessen": window.top.borderCoordinates = states[6].geometry.coordinates[0]; break;
+                    case "Mecklenburg-Vorpommern": window.top.borderCoordinates = states[7].geometry.coordinates[0]; break;
+                    case "Niedersachsen": window.top.borderCoordinates = states[8].geometry.coordinates[0]; break;
+                    case "Nordrhein-Westfalen": window.top.borderCoordinates = states[9].geometry.coordinates[0]; break;
+                    case "Rheinland-Pfalz": window.top.borderCoordinates = states[10].geometry.coordinates[0]; break;
+                    case "Saarland": window.top.borderCoordinates = states[11].geometry.coordinates[0]; break;
+                    case "Sachsen-Anhalt": window.top.borderCoordinates = states[12].geometry.coordinates[0]; break;
+                    case "Sachsen": window.top.borderCoordinates = states[13].geometry.coordinates[0]; break;
+                    case "Schleswig-Holstein": window.top.borderCoordinates = states[14].geometry.coordinates[0]; break;
+                    case "Thüringen": window.top.borderCoordinates = states[15].geometry.coordinates[0]; break;
                     default: break;
                 }
-                console.log(borderCoordinates);
+                setLS(window.top.borderCoordinates);
             });
         });
 
 
     }
     if (url.includes("mission")) {
-        console.log("Here we go");
-        console.log(borderCoordinates);
         var missionId = url.split("/")[2];
         var missionUrl = window.top.document.getElementById("mission_caption_" + missionId);
         var missionLatitude = missionUrl.getAttribute("data-latitude");
         var missionLongitude = missionUrl.getAttribute("data-longitude");
+        window.top.borderCoordinates = getLS();
         var missionCoordinates = [missionLongitude, missionLatitude];
-        var missionIsInPolygon = inside(missionCoordinates, borderCoordinates);
+        if(window.top.borderCoordinates == null && !useOwnCoordinates){
+            alert("Keine Grenze angegeben! Alle Einsätze werden außerhalb des Gebiets deklariert!");
+            window.top.borderCoordinates = [[0, 0], [0, 0]];
+        }
+        var missionIsInPolygon = inside(missionCoordinates, window.top.borderCoordinates);
+        if (!missionIsInPolygon) {
+            var shareBtn = document.getElementById("mission_alliance_share_btn");
+            shareBtn.classList.add("btn-warning");
+            shareBtn.classList.remove("btn-success")
+            var shareAlarmForwardBtn = document.getElementsByClassName("alert_next_alliance");
+            for (var i = 0; i < shareAlarmForwardBtn.length; i++) {
+                shareAlarmForwardBtn[i].classList.add("btn-warning");
+                shareAlarmForwardBtn[i].classList.remove("btn-success");
+
+            }
+        }
 
     }
 
 
     function inside(point, vs) {
         // ray-casting algorithm based on
-        // https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html/pnpoly.html
+        // https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html
 
         var x = point[0], y = point[1];
 
@@ -111,19 +129,12 @@
         return inside;
     };
 
-    if (url.includes("mission")) {
-        if (!missionIsInPolygon) {
-            var shareBtn = document.getElementById("mission_alliance_share_btn");
-            shareBtn.classList.add("btn-warning");
-            shareBtn.classList.remove("btn-success")
-            var shareAlarmForwardBtn = document.getElementsByClassName("alert_next_alliance");
-            for (var i = 0; i < shareAlarmForwardBtn.length; i++) {
-                shareAlarmForwardBtn[i].classList.add("btn-warning");
-                shareAlarmForwardBtn[i].classList.remove("btn-success");
+    function getLS() {
+    return JSON.parse(localStorage.getItem('LSS_mission_in_area_check'));
+    };
 
-            }
-        }
+    function setLS(value) {
+        localStorage.setItem('LSS_mission_in_area_check', JSON.stringify(value));
     }
-
 
 })();
